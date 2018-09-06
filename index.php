0d0333f5d31486e15a5e5ed9f92f60df1514283d
Bennett Warner <bennettdwarner@gmail.com>
2018-09-06 14:37:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,7 @@
     <br>
     <h1 class="text-center">Server Status</h1>
     <br>
-    <?php
-    makeTable(getHosts());
-    ?>
+    <?php makeTable(getHosts()); ?>
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
